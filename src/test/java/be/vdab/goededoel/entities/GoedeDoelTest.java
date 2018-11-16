08fd1faaed18dc880f5cd0353cb36bbc0fca953f
Michelle Norico <kazekage422@gmail.com>
2018-11-16 13:43:17 +0100

    commit met extra test in GoedeDoelTest

--- src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
@@ -1,6 +1,7 @@
 package be.vdab.goededoel.entities;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.math.BigDecimal;
 
@@ -26,5 +27,15 @@ public class GoedeDoelTest {
 	@Test
 	public void eenNieuwDoelHeeftNogNietsOpgebracht() {
 		assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
+	}
+
+	@Test
+	public void goedeDoelenMetDezelfdeNaamMoetenVolgensEqualsGelijkZijn() {
+		assertEquals(new GoedeDoel("CLINICLOWNS"), doel);
+	}
+
+	@Test 
+	public void goedeDoelenMetVerschillendeNaamMoetenVolgensEqualsVerschillendZijn() {
+		assertNotEquals(new GoedeDoel("Unicef"), doel);   
 	}
 }
